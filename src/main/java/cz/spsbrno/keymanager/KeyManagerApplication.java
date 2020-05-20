dfeb0b0a13364eb85efff69701dd2c947e8fd07b
K <tobias.technicky@gmail.com>
2020-05-20 22:35:56 +0200

    init stringboot app

---
+++ src/main/java/cz/spsbrno/keymanager/KeyManagerApplication.java
@@ -0,0 +1,13 @@
+package cz.spsbrno.keymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/spsbrno/keymanager/KeyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.spsbrno.keymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
